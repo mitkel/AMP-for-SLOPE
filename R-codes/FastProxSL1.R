928da3f44f1576ef823a2ee73e649862a3946ed5
mitkel <polaczykb@gmail.com>
2020-07-10 18:22:38 +0200

    added FastProxSL1 algorithm

--- R-codes/FastProxSL1.R
+++ R-codes/FastProxSL1.R
@@ -26,12 +26,42 @@ RetrV <- function( w, sgn, P ){
 }
 
 # sanity check
-foo <- rnorm(20)
-bar <- MapToDelta(foo)
-print( data.frame("v" = foo, "w" = bar$w) )
-print( all.equal((foo*bar$sgn)[bar$P], bar$w) )
-print( all.equal(RetrV(bar$w, bar$sgn, bar$P), foo) )
+# foo <- rnorm(10)
+# bar <- MapToDelta(foo)
+# print( data.frame("v" = foo, "w" = bar$w) )
+# print( isTRUE( all.equal((foo*bar$sgn)[bar$P], bar$w) ) )
+# print( isTRUE( all.equal(RetrV(bar$w, bar$sgn, bar$P), foo) ) )
 
-FastProxSL1 <- function( y, theta ){
-  
+
+# DeltaCheck:
+#   - since MapToDelta() requires a sorting step, we want a faster method of checking if v in Delta_p
+#   Input: vector v
+#   Output: TRUE iff v in Delta_p
+DeltaCheck <- function( v ){
+  # return( isTRUE(all.equal(v, MapToDelta(v)$w )) )
+  return( max(diff(v)) <= 0 && min(v) >= 0  )
 }
+
+# FastProxSL1:
+#   Input: vect. v, vect. theta
+#   Output: argmin_x { |x-y|^2/2 + phi_theta(x) } = (y-theta)_+ if (y-theta) in Delta_p
+FastProxSL1 <- function( y, theta){
+  if( !DeltaCheck(theta) ) warning("Arg. theta is not in Delta_p!") 
+  foo <- MapToDelta(y)
+  y <- foo$w
+  bar <- as.numeric( lapply( y-theta, function(x) max(x,0) ) )
+  
+  while( !DeltaCheck(bar) ){
+   i <- which(diff(y-theta) > 0)[1] # begining of the first nondecreasing subsequence in y-theta
+   j <- i + which(diff( (y-theta)[-(1:i)] ) < 0)[1] # end of the first nondecreasing subsequence in y-theta
+   y[i:j] <- mean(y[i:j])
+   theta[i:j] <- mean(theta[i:j])
+   bar <- as.numeric( lapply( y-theta, function(x) max(x,0) ) )
+  }
+  return( RetrV(bar, foo$sgn, foo$P))
+}
+
+# sanity check:
+# for theta = const, ProxSL1 is a shrinkage operator
+print(foo <- sample(seq(from=-5, to=5, by=1), 11))
+print(FastProxSL1(foo, theta=rep(2, times=11)))
